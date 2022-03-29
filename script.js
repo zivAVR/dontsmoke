f90f1ff56624fa96ad9461c57752f8f52c544a33
zivAVR <96915452+zivAVR@users.noreply.github.com>
2022-03-29 20:22:31 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const menuIcon = document.querySelector('.hamburger-menu');
+const navbar = document.querySelector('.navbar')
+menuIcon.addEventListener('click', () => {
+    navbar.classList.toggle('change');
+})
